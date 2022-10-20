410df529ba80a2bb7108a028df35ea1b9bb28a85
juba0x00 <mahmoudhawash90@gmail.com>
2022-10-20 14:26:11 +0200

    fix too many files open

--- SFTPS/SFTPS.py
+++ SFTPS/SFTPS.py
@@ -1,33 +1,30 @@
 # SFTPS: Simple Fast TCP Port Scanner
+# SFTPS IP START_PORT END_PORT
 import socket 
-from sys import argv 
-from cProfile import Profile 
-import pstats 
+from sys import argv, exit
 from threading import Thread 
-# STPS IP START_PORT END_PORT
+from resource import getrlimit, RLIMIT_NOFILE, setrlimit
+
 
 if len(argv) < 4:
-	print('Usage: STPS IP START_PORT END_PORT')
-	quit()
+	print('Usage: SFTPS IP START_PORT END_PORT')
+	exit(0)
+
+
+soft_limit, hard_limit = getrlimit(RLIMIT_NOFILE)
+setrlimit(RLIMIT_NOFILE, (int(argv[3]) - int(argv[2]), hard_limit))
+
 
 
 ip_addr = argv[1]
 
 def check(port):
-
-	if socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip_addr, port)) == 0: # 111 connection refused, 0 connected
-		print(port)
-
-def main():
+    if socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip_addr, port)) == 0: # 111 connection refused, 0 connected
+        print(port)
+        
+    
+        
+if __name__ == '__main__':
 	for port in range(int(argv[2]), int(argv[3]) + 1):
 		Thread(target=check, args=[port]).start()
-
-
-if __name__ == '__main__':
-    with Profile() as pr:
-        main()
-    
-    stats = pstats.Stats(pr)
-    stats.sort_stats(pstats.SortKey.TIME)
-    # stats.print_stats()
-    stats.dump_stats(filename='profiling_threading.prof')
+      
