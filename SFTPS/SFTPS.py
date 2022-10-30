7d4b1fc616425ada9cb147a1e8a6fec0b6d7e340
juba0x00 <mahmoudhawash90@gmail.com>
2022-10-30 09:58:02 +0200

    passing output to nmap

--- SFTPS/SFTPS.py
+++ SFTPS/SFTPS.py
@@ -1,30 +1,31 @@
 # SFTPS: Simple Fast TCP Port Scanner
 # SFTPS IP START_PORT END_PORT
-import socket 
+import socket
 from sys import argv, exit
-from threading import Thread 
+from threading import Thread
 from resource import getrlimit, RLIMIT_NOFILE, setrlimit
 
+if len(argv) < 2:
+    print(f'Usage: {argv[0]} IP')
+    exit(0)
 
-if len(argv) < 4:
-	print('Usage: SFTPS IP START_PORT END_PORT')
-	exit(0)
-
-
+# increase file descriptors
 soft_limit, hard_limit = getrlimit(RLIMIT_NOFILE)
-setrlimit(RLIMIT_NOFILE, (int(argv[3]) - int(argv[2]), hard_limit))
-
-
+setrlimit(RLIMIT_NOFILE, (65535, hard_limit))
 
 ip_addr = argv[1]
+open_ports = []
 
-def check(port):
-    if socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip_addr, port)) == 0: # 111 connection refused, 0 connected
-        print(port)
-        
-    
-        
+
+def check(crnt_port):
+    if socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((ip_addr, crnt_port)) == 0:
+        # 111 connection refused, 0 connected
+        # print(f'{crnt_port}')
+        open_ports.append(crnt_port)
+
+
 if __name__ == '__main__':
-	for port in range(int(argv[2]), int(argv[3]) + 1):
-		Thread(target=check, args=[port]).start()
-      
+    for port in range(65535):
+        Thread(target=check, args=[port]).start()
+
+    print(*open_ports, sep=',')
